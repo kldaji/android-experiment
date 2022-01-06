b35137ad4fadac9255be418c1052f22bd8db84d8
KLDAJI <kldaji@naver.com>
2022-01-06 15:43:25 +0900

    #2 feat: 프래그먼트 추가 리스너 구현

--- app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.kldaji.android_experiment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kldaji.android_experiment.databinding.ActivityMainBinding
+import com.kldaji.android_experiment.multifragment.OneFragment
+import com.kldaji.android_experiment.multifragment.TwoFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val oneFragment = OneFragment()
+    private val twoFragment = TwoFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setAddFragmentListener()
+    }
+
+    private fun setAddFragmentListener() {
+        binding.btnMainAdd1.setOnClickListener {
+            with(supportFragmentManager.beginTransaction()) {
+                replace(R.id.fc_main_1, oneFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.btnMainAdd2.setOnClickListener {
+            with(supportFragmentManager.beginTransaction()) {
+                replace(R.id.fc_main_2, twoFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
