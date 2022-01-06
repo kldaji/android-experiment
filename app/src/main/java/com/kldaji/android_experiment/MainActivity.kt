ec4e0d66a70c540bf57d587880200894a7cb28b3
KLDAJI <kldaji@naver.com>
2022-01-06 21:09:48 +0900

    #2 feat: fragment-ktx 적용하여 transaction 실행 및 fragment show, hide 기능 추가

--- app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
@@ -2,13 +2,14 @@ package com.kldaji.android_experiment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.kldaji.android_experiment.databinding.ActivityMainBinding
 import com.kldaji.android_experiment.multifragment.OneFragment
 import com.kldaji.android_experiment.multifragment.TwoFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val oneFragment = OneFragment()
     private val twoFragment = TwoFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,21 +17,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setAddFragmentListener()
+        setShowFragmentListener()
+        setHideFragmentListener()
     }
 
     private fun setAddFragmentListener() {
         binding.btnMainAdd1.setOnClickListener {
-            with(supportFragmentManager.beginTransaction()) {
-                replace(R.id.fc_main_1, oneFragment)
+            supportFragmentManager.commit {
+                replace<OneFragment>(R.id.fc_main_1)
                 addToBackStack(null)
-                commit()
             }
         }
         binding.btnMainAdd2.setOnClickListener {
-            with(supportFragmentManager.beginTransaction()) {
+            supportFragmentManager.commit {
                 replace(R.id.fc_main_2, twoFragment)
                 addToBackStack(null)
-                commit()
+            }
+        }
+    }
+
+    private fun setShowFragmentListener() {
+        binding.btnMainShow2.setOnClickListener {
+            supportFragmentManager.commit {
+                show(twoFragment)
+            }
+        }
+    }
+
+    private fun setHideFragmentListener() {
+        binding.btnMainHide2.setOnClickListener {
+            supportFragmentManager.commit {
+                hide(twoFragment)
             }
         }
     }
