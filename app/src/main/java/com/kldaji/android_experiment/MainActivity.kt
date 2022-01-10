d919dec2516d7655af61e56f6c8ea101e8a2d302
KLDAJI <kldaji@naver.com>
2022-01-10 10:39:11 +0900

    #3 feat: Toast, Start Activity, Dialog 기능 추가

--- app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_experiment/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.kldaji.android_experiment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kldaji.android_experiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setToastButtonListener()
+        setSubButtonListener()
+//        val dialog = AlertDialog.Builder(applicationContext)
+//            .setMessage("123")
+//            .create()
+//            .show()
+    }
+
+    private fun setToastButtonListener() {
+        binding.btnMainToast.setOnClickListener {
+            Toast.makeText(this, "토스트 메세지", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setSubButtonListener() {
+        binding.btnMainSub.setOnClickListener {
+            startActivity(Intent(applicationContext,SubActivity::class.java))
+        }
     }
 }
