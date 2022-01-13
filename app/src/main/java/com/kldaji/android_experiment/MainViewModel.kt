fe480d45be10bcd3537d98de06b9842505c7f1aa
KLDAJI <kldaji@naver.com>
2022-01-13 11:36:45 +0900

    #4 feat: concurrency 보장하기 위해 Mutex 추가

--- app/src/main/java/com/kldaji/android_experiment/MainViewModel.kt
+++ app/src/main/java/com/kldaji/android_experiment/MainViewModel.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class MainViewModel : ViewModel() {
     var count = 0
+    private val mutex = Mutex()
 
     fun start(callback: () -> Unit) {
         for (i in 0..1000) {
             viewModelScope.launch(Dispatchers.IO) {
-                count++
+                mutex.withLock {
+                    count++
+                }
             }
         }
         viewModelScope.launch {
